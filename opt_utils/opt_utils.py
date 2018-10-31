67c8dbada155e0e0b4fe61768d01cd4c30395d8b
miguelriemoliveira <m.riem.oliveira@gmail.com>
2018-10-31 12:13:41 +0000

    initial commit

---
+++ opt_utils/opt_utils.py
@@ -0,0 +1,52 @@
+"""
+This is a long, multiline description
+"""
+
+import argparse  # to read command line arguments
+#to list images in a folder
+#import glob
+#import json
+#import shutil
+#import yaml
+#from collections import namedtuple
+#from copy import deepcopy
+#from matplotlib import cm
+#import openmesh as om
+#from tqdm import tqdm
+#from model_viewer import *
+#from rgbd_camera import *
+#from itertools import combinations
+from scipy.spatial import distance  #Compute euclidean distance
+from scipy.sparse import lil_matrix 
+from scipy.optimize import least_squares
+
+# ------------------------
+##   DATA STRUCTURES   ##
+# ------------------------
+
+# ------------------------
+## FUNCTION DEFINITION ##
+# ------------------------
+
+# ------------------------
+###   BASE CLASSES    ###
+# ------------------------
+
+# ------------------------
+### CLASS DEFINITION  ###
+# ------------------------
+
+class Optimizer():
+    """ Class to color correct
+    """
+
+    def __init__(self):
+        """Implements a color corrector
+           Args: 
+           Returns:
+              None for now 
+        """
+
+    def setStaticData(data):
+        self.data = data
+
---
+++ test/test.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+"""
+This is a long, multiline description
+"""
+
+#-------------------------------------------------------------------------------
+#--- Add rgbd_tm module to the sys.path
+#--- Will probably have to be done for the code which is using this API
+#-------------------------------------------------------------------------------
+#import sys
+#import os.path
+#sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+#-------------------------------------------------------------------------------
+#--- IMPORTS (standard, then third party, then my own modules)
+#-------------------------------------------------------------------------------
+import argparse  #to read command line arguments
+
+import opt_utils.opt_utils as opt_utils
+
+#-------------------------------------------------------------------------------
+#--- HEADER
+#-------------------------------------------------------------------------------
+__author__ = "Miguel Riem de Oliveira"
+__date__ = "2018"
+__copyright__ = "Miguel Riem de Oliveira"
+__credits__ = ["Miguel Riem de Oliveira"]
+__license__ = ""
+__version__ = "1.0"
+__maintainer__ = "Miguel Oliveira"
+__email__ = "m.riem.oliveira@gmail.com"
+__status__ = "Development"
+
+#-------------------------------------------------------------------------------
+#--- FUNCTIONS
+#-------------------------------------------------------------------------------
+
+
+#-------------------------------------------------------------------------------
+#--- MAIN
+#-------------------------------------------------------------------------------
+if __name__ == "__main__":
+
+    #---------------------------------------
+    #--- Parse command line argument
+    #---------------------------------------
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-p", "--path_to_images", help = "path to the folder that contains the images", required=True)
+    args = vars(ap.parse_args())
+
+    #---------------------------------------
+    #--- INITIALIZATION
+    #---------------------------------------
+    optimizer = opt_utils.Optimizer()
+
+
+
+    #---------------------------------------
+    #--- USING THE USER INTERFACE
+    #---------------------------------------
+
+
+    #---------------------------------------
+    #--- USING THE API
+    #---------------------------------------
+
+
