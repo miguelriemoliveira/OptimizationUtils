93d4c5f0bb27fcb015f9044b336841a4d80096c3
miguelriemoliveira <m.riem.oliveira@gmail.com>
2018-11-12 00:51:12 +0000

    close #1. Implemented several functionalities described above.

--- OptimizationUtils/OptimizationUtils.py
+++ OptimizationUtils/OptimizationUtils.py
@@ -1,111 +1,168 @@
-"""
-This is a long, multiline description
-"""
-
-import argparse  # to read command line arguments
-#to list images in a folder
-import random
-#import glob
-#import json
-#import shutil
-#import yaml
-#from collections import namedtuple
-#from copy import deepcopy
-#from matplotlib import cm
-#import openmesh as om
-#from tqdm import tqdm
-#from model_viewer import *
-#from rgbd_camera import *
-#from itertools import combinations
-from scipy.spatial import distance  #Compute euclidean distance
-from scipy.sparse import lil_matrix 
+# -------------------------------------------------------------------------------
+# --- IMPORTS (standard, then third party, then my own modules)
+# -------------------------------------------------------------------------------
+from collections import namedtuple, OrderedDict
+from copy import deepcopy
+import pandas
+import cv2
+from numpy import inf
 from scipy.optimize import least_squares
+from scipy.sparse import lil_matrix
 
 # ------------------------
 ##   DATA STRUCTURES   ##
 # ------------------------
-from collections import namedtuple
 ParamT = namedtuple('ParamT', 'idx data_key getter setter')
 
-# ------------------------
-## FUNCTION DEFINITION ##
-# ------------------------
 
-# ------------------------
-###   BASE CLASSES    ###
-# ------------------------
-
-# ------------------------
-### CLASS DEFINITION  ###
-# ------------------------
-
-class Optimizer():
-    """ Class to color correct
-    """
+# -------------------------------------------------------------------------------
+# --- FUNCTIONS
+# -------------------------------------------------------------------------------
+class Optimizer:
 
     def __init__(self):
-        """Implements a color corrector
-           Args: 
-           Returns:
-              None for now 
+        self.counter = 0
+        self.model = {}  # a dict with a set of variables or structures to be used by the objective function
+        self.params = OrderedDict()  # an ordered dict where key={name} and value = namedtuple('ParamT')
+        self.x = []  # a list of floats (the actual parameters)
+        self.x0 = []  # the initial value of the parameters
+        self.bound_min = []  # min values for x
+        self.bound_max = []  # max values for x
+        self.residuals = []
+        self.residual_params = {}
+
+    def setFirstGuess(self):
+        self.x0 = deepcopy(self.x)
+
+    def pushScalarParam(self, name, model_key, getter, setter, bound_max=+inf, bound_min=-inf):
         """
-        self.static_data = {}
-        self.params = {}    
-        self.x = []
+        Pushes a new parameter to the parameter vector
+        :param name: the name of the parameter
+        :param model_key: the key of the model into which the parameter maps
+        :param getter: a function to retrieve the parameter value from the model
+        :param setter: a function to set the parameter value from the model
+        :param bound_max: max value the parameter may take
+        :param bound_min: min value the parameter may take
+        """
+        if name in self.params:  # Cannot add a parameter that already exists
+            raise ValueError('Scalar param ' + name + ' already exists.')
 
-    def pushScalarParam(self, name, data_key, getter, setter):
-
-        if name in self.params:
-            raise ValueError('Scalar param ' + name + ' already exits.')
-
-        self.params[name] = ParamT(len(self.x), data_key, getter, setter)
-        self.x.append(getter(self.static_data[data_key]))
+        self.params[name] = ParamT(len(self.x), model_key, getter, setter)  # add to params dict
+        self.x.append(getter(self.model[model_key]))  # set initial value in x
+        self.bound_min.append(bound_min)  # set min value for this param
+        self.bound_max.append(bound_max)  # set max value for this param
 
         print('Pushed scalar param ' + name)
 
+    def pushResidual(self, name, params=None):
+        """
+        Adds a new residual to the existing list of residuals
+        :param name: name of residual
+        :param params: list of parameter names which affect this residual
+        """
+        self.residuals.append(name)
+        self.residual_params[name] = params
+
     def fromModelToX(self):
+        """Copies values of all parameters from model to vector x"""
         for name in self.params:
             idx, data_key, getter, _ = self.params[name]
-            value = getter(self.static_data[data_key])
+            value = getter(self.model[data_key])
             self.x[idx] = value
 
-    def fromXToModel(self):
+    def fromXToModel(self, x=None):
+        """Copies values of all parameters from vector x to model"""
+        if x is None:
+            x = self.x
+
         for name in self.params:
-            print("from x to model param " + str(name))
             idx, data_key, _, setter = self.params[name]
-            value = self.x[idx]
-            print('value = ' + str(value))
-            print('setter = ' + str(setter))
-            setter(self.static_data[data_key], value)         
+            value = x[idx]
+            setter(self.model[data_key], value)
 
     def addStaticData(self, name, data):
         """ Should be a dictionary containing every static data to be used by the cost function"""
-        print('Adding new data "' + name + '" to static data')
-        self.static_data[name] = data
+        if name in self.params:  # Cannot add a parameter that already exists
+            raise ValueError(name + ' already exits in model dict.')
+        else:
+            self.model[name] = data
+            print('Added ' + name + ' to model dict.')
 
+    # noinspection PyAttributeOutsideInit
     def setObjectiveFunction(self, function_handle):
         self.objective_function = function_handle
 
     def callObjectiveFunction(self):
-        self.objective_function(self.static_data)
+        self.objective_function(self.model)
+
+    # noinspection PyAttributeOutsideInit
+    def setVisualizationFunction(self, function_handle, n_iterations=0):
+        self.visualization_function = function_handle
+        self.visualization_function_iterations = n_iterations
+
+    # noinspection PyAttributeOutsideInit
+    def computeSparseMatrix(self):
+        self.sparse_matrix = lil_matrix((len(self.residuals), len(self.params)), dtype=int)
+
+        for i, residual in enumerate(self.residuals):
+            for param in self.residual_params[residual]:
+                idx, _, _, _ = self.params[param]
+                self.sparse_matrix[i, idx] = 1
+
+        print('Sparsity matrix:')
+        print(pandas.DataFrame(self.sparse_matrix.toarray(), self.residuals, self.params.keys()))
+
+    def printX(self, text='Parameter vector:', x=None):
+        if x is None:
+            x = self.x
+
+        print(text)
+        for i, param_value in enumerate(x):
+            for name in self.params:
+                idx, _, _, _ = self.params[name]
+                if idx == i:
+                    print('   ' + name + ' = ' + str(param_value))
+
+    def printModel(self):
+        print('Model =\n' + str(self.model))
+
+    def printXAndModel(self, opt):
+        self.printX()
+        self.printModel()
 
     def internalObjectiveFunction(self, x):
+        """
+        A wrapper around the custom given objective function which maps the x vector to the model before calling the
+        objetive function and after the call
+        :param x: the parameters vector
+        """
         self.x = x
         self.fromXToModel()
-        error = self.objective_function(self.static_data)
+        error = self.objective_function(self.model)
+
+        if self.counter >= self.visualization_function_iterations:
+            self.visualization_function(self.model)
+            self.counter = 0
+        self.counter += self.counter
+
         return error
 
-    def printXandModel(self, opt):
-        print('-----')
-        print('x0 = ' + str(self.x))
-        for i, camera in enumerate(self.static_data['dataset'].cameras):
-            print('camera ' + str(i) + ' = ' + str(camera.rgb.bias))
-        print('-----')
+    def startOptimization(self, optimization_options={'x_scale': 'jac', 'ftol': 1e-6, 'xtol': 1e-6, 'gtol': 1e-8,
+                                                      'diff_step': 1e0}):
+        self.setFirstGuess()
+        bounds = (self.bound_min, self.bound_max)
+        self.result = least_squares(self.internalObjectiveFunction, self.x, verbose=2, jac_sparsity=self.sparse_matrix,
+                                    bounds=bounds, method='trf', args=(), **optimization_options)
+        self.xf = list(self.result.x)
+        self.finalOptimizationReport()
 
-    def startOptimization(self):
-        # res = least_squares(costFunction, x0, verbose=2, jac_sparsity=A, x_scale='jac', ftol=1e-4, xtol=1e-4, bounds=bounds, method='trf', args=(X, Pc, detections, args, handles, handle_fun, s))
+    def finalOptimizationReport(self):
+        """Just print some info and show the images"""
+        print('\n-------------\nOptimization finished')
+        print(self.result)
+        self.printX(text='\nInitial value of parameters', x=self.x0)
+        self.printX(text='\nFinal value of parameters', x=self.xf)
 
-        res = least_squares(self.internalObjectiveFunction, self.x, verbose=2, x_scale='jac', ftol=1e-6, xtol=1e-6, gtol=1e-6, diff_step=1e0,  method='trf', args=())
-
-
-
+        self.fromXToModel(self.xf)
+        self.visualization_function(self.model)
+        cv2.waitKey(0)
--- test/color_balancing_oc_dataset.py
+++ test/color_balancing_oc_dataset.py
@@ -3,23 +3,21 @@
 This is a long, multiline description
 """
 
-#-------------------------------------------------------------------------------
-#--- IMPORTS (standard, then third party, then my own modules)
-#-------------------------------------------------------------------------------
-import argparse  #to read command line arguments
+# -------------------------------------------------------------------------------
+# --- IMPORTS (standard, then third party, then my own modules)
+# -------------------------------------------------------------------------------
+import argparse  # to read command line arguments
 from itertools import combinations
 import numpy as np
-from copy import deepcopy
 import cv2
 from functools import partial
 import random
-
 import OCDatasetLoader.OCDatasetLoader as OCDatasetLoader
 import OptimizationUtils.OptimizationUtils as OptimizationUtils
 
-#-------------------------------------------------------------------------------
-#--- FUNCTIONS
-#-------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
+# --- FUNCTIONS
+# -------------------------------------------------------------------------------
 
 def keyPressManager(self):
     print('keyPressManager.\nPress "c" to continue or "q" to abort.')
@@ -32,63 +30,64 @@ def keyPressManager(self):
             print('Pressed "q". Aborting.')
             exit(0)
 
-def addSafe(i_in, val):
-        """Avoids saturation when adding to uint8 images"""
-        i_out = i_in.astype(np.float) #Convert the i to type float 
-        i_out = np.add(i_out, val) #Perform the adding of parameters to the i
-        i_out = np.maximum(i_out, 0)   #underflow
-        i_out = np.minimum(i_out, 255) #overflow
-        return i_out.astype(np.uint8) #Convert back to uint8 and return
 
-#-------------------------------------------------------------------------------
-#--- MAIN
-#-------------------------------------------------------------------------------
+def addSafe(i_in, val):
+    """Avoids saturation when adding to uint8 images"""
+    i_out = i_in.astype(np.float)  # Convert the i to type float
+    i_out = np.add(i_out, val)  # Perform the adding of parameters to the i
+    i_out = np.maximum(i_out, 0)  # underflow
+    i_out = np.minimum(i_out, 255)  # overflow
+    return i_out.astype(np.uint8)  # Convert back to uint8 and return
+
+
+# -------------------------------------------------------------------------------
+# --- MAIN
+# -------------------------------------------------------------------------------
 if __name__ == "__main__":
 
-    #---------------------------------------
-    #--- Parse command line argument
-    #---------------------------------------
+    # ---------------------------------------
+    # --- Parse command line argument
+    # ---------------------------------------
     ap = argparse.ArgumentParser()
 
-    #Dataset loader arguments
+    # Dataset loader arguments
     ap.add_argument("-p", "--path_to_images", help="path to the folder that contains the OC dataset", required=True)
     ap.add_argument("-ext", "--image_extension", help="extension of the image files, e.g., jpg or png", default='jpg')
     ap.add_argument("-m", "--mesh_filename", help="full filename to input obj file, i.e. the 3D model", required=True)
     ap.add_argument("-i", "--path_to_intrinsics", help="path to intrinsics yaml file", required=True)
-    ap.add_argument("-ucci", "--use_color_corrected_images", help="Use previously color corrected images", action='store_true', default=False)
+    ap.add_argument("-ucci", "--use_color_corrected_images", help="Use previously color corrected images",
+                    action='store_true', default=False)
     ap.add_argument("-si", "--skip_images", help="skip images. Useful for fast testing", type=int, default=1)
-    ap.add_argument("-vri", "--view_range_image", help="visualize sparse and dense range images", action='store_true', default=False)
-    
-    #OptimizationUtils arguments
-    ap.add_argument("-sv", "--skip_vertices", help="skip vertices. Useful for fast testing", type=int, default=1)
-    ap.add_argument("-z", "--z_inconsistency_threshold", help="threshold for max z inconsistency value", type=float, default=0.05)
-    ap.add_argument("-vpv", "--view_projected_vertices", help="visualize projections of vertices onto images", action='store_true', default=False)
-    ap.add_argument("-vo", "--view_optimization", help="...", action='store_true', default=False)
+    ap.add_argument("-vri", "--view_range_image", help="visualize sparse and dense range images", action='store_true',
+                    default=False)
 
+    # OptimizationUtils arguments
+    ap.add_argument("-sv", "--skip_vertices", help="skip vertices. Useful for fast testing", type=int, default=1)
+    ap.add_argument("-z", "--z_inconsistency_threshold", help="threshold for max z inconsistency value", type=float,
+                    default=0.05)
+    ap.add_argument("-vpv", "--view_projected_vertices", help="visualize projections of vertices onto images",
+                    action='store_true', default=False)
+    ap.add_argument("-vo", "--view_optimization", help="...", action='store_true', default=False)
 
     args = vars(ap.parse_args())
     print(args)
 
-    #---------------------------------------
-    #--- INITIALIZATION
-    #---------------------------------------
+    # ---------------------------------------
+    # --- INITIALIZATION
+    # ---------------------------------------
     dataset_loader = OCDatasetLoader.Loader(args)
     dataset = dataset_loader.loadDataset()
     num_cameras = len(dataset.cameras)
     print(num_cameras)
 
-    #Change camera's colors just to better see optimization working
+    # Change camera's colors just to better see optimization working
     for i, camera in enumerate(dataset.cameras):
         dataset.cameras[i].rgb.image = addSafe(dataset.cameras[i].rgb.image, random.randint(-70, 70))
 
-
-    #lets add a bias variable to each camera.rgb. This value will be used to change the image and optimize
+    # lets add a bias variable to each camera.rgb. This value will be used to change the image and optimize
     for i, camera in enumerate(dataset.cameras):
         # camera.rgb.bias = random.randint(-30, 30)
-        camera.rgb.bias = 50
-
-    
-        
+        camera.rgb.bias = 0
 
     # ---------------------------------------
     # --- Setup Optimizer
@@ -98,78 +97,95 @@ if __name__ == "__main__":
     opt.addStaticData('dataset', dataset)
     opt.addStaticData('another_thing', [])
 
-      
+
     def setter(dataset, value, i):
         dataset.cameras[i].rgb.bias = value
+
 
     def getter(dataset, i):
         return dataset.cameras[i].rgb.bias
 
+
     # Create specialized getter and setter functions
     for idx_camera, camera in enumerate(dataset.cameras):
-        opt.pushScalarParam('bias' + str(idx_camera), 'dataset', partial(getter,i=idx_camera), partial(setter,i=idx_camera))
+        if idx_camera == 0:
+            bound_max = camera.rgb.bias + 0.00001
+            bound_min = camera.rgb.bias - 0.00001
+        else:
+            bound_max = camera.rgb.bias + 50
+            bound_min = camera.rgb.bias - 50
 
-    #TODO max and min limits
-    #TODO sparse matrix
-
-
+        opt.pushScalarParam(name='bias_' + camera.name, model_key='dataset', getter=partial(getter, i=idx_camera),
+                            setter=partial(setter, i=idx_camera), bound_max=bound_max, bound_min=bound_min)
 
     # ---------------------------------------
-    # --- SET THE OBJECTIVE FUNCTION
+    # --- Define THE OBJECTIVE FUNCTION
     # ---------------------------------------
-    
+    def objectiveFunction(model):
 
-    def objectiveFunction(static_data, show=False):
+        # Get the dataset from the model dictionary
+        dataset = model['dataset']
 
-        #Get the static data from the dictionary
-        dataset = static_data['dataset']
-        cameras = dataset.cameras
-        num_cameras = len(cameras)
+        # Apply changes to all camera images using parameter vector
+        for camera in dataset.cameras:
+            camera.rgb.image_changed = addSafe(camera.rgb.image, camera.rgb.bias)
+            camera.rgb.avg_changed = np.average(camera.rgb.image_changed)
 
-        #Apply changes to all camera images using parameter vector
-        changed_images = []
-        for i in range(0,num_cameras):
-            changed_images.append(addSafe(cameras[i].rgb.image, cameras[i].rgb.bias))
-
-        #Compute averages for initial and changed images
-        initial_avgs = np.zeros((num_cameras))
-        changed_avgs = np.zeros((num_cameras))
-        for i in range(0,num_cameras):
-            initial_avgs[i] = np.average(cameras[i].rgb.image)
-            changed_avgs[i] = np.average(changed_images[i])
-
-        #Compute error as the sum of differences between initial and changed averages. Error is a scalar in this example, hence the sum
-        error = np.sum(initial_avgs - changed_avgs)
-
-        #Visualization
-        for i in range(0,num_cameras):
-            cv2.namedWindow('Initial Cam ' + str(i), cv2.WINDOW_NORMAL)
-            cv2.imshow('Initial Cam ' + str(i), cameras[i].rgb.image)
-            cv2.namedWindow('Changed Cam ' + str(i), cv2.WINDOW_NORMAL)
-            cv2.imshow('Changed Cam ' + str(i), changed_images[i])
-        cv2.waitKey(0)
+        # Compute all the pair wise combinations of the set of cameras
+        # Each element in the vector of errors is the difference of the average color for the combination
+        error = []
+        for cam_a, cam_b in combinations(dataset.cameras, 2):
+            error.append(cam_a.rgb.avg_changed - cam_b.rgb.avg_changed)
 
         return error
-    # -----------OBJECTIVE FUNCTION FINISHED ------------------
-    
+
+
     opt.setObjectiveFunction(objectiveFunction)
 
-    #---------------------------------------
-    #--- Create X0 (First Guess)
-    #---------------------------------------
+    # ---------------------------------------
+    # --- Define THE RESIDUALS
+    # ---------------------------------------
+    # The error is computed from the difference of the average color of one image with another
+    # Thus, we will use all pairwise combinations of available images
+    # For example, if we have 3 cameras c0, c1 and c2, the residuals should be:
+    #    c0-c1, c0-c2, c1-c2
 
+    for cam_a, cam_b in combinations(dataset.cameras, 2):
+        opt.pushResidual(name='c' + cam_a.name + '-c' + cam_b.name, params=['bias_' + cam_a.name, 'bias_' + cam_b.name])
+
+    print('residuals = ' + str(opt.residuals))
+    print('residual_params = ' + str(opt.residual_params))
+
+    opt.computeSparseMatrix()
+
+
+    # ---------------------------------------
+    # --- Define THE VISUALIZATION FUNCTION
+    # ---------------------------------------
+    def visualizationFunction(model):
+        # Get the dataset from the model dictionary
+        dataset = model['dataset']
+
+        for i, camera in enumerate(dataset.cameras):
+            cv2.namedWindow('Initial Cam ' + str(i), cv2.WINDOW_NORMAL)
+            cv2.imshow('Initial Cam ' + str(i), camera.rgb.image)
+
+        for i, camera in enumerate(dataset.cameras):
+            cv2.namedWindow('Changed Cam ' + str(i), cv2.WINDOW_NORMAL)
+            cv2.imshow('Changed Cam ' + str(i), camera.rgb.image_changed)
+        cv2.waitKey(20)
+
+
+    opt.setVisualizationFunction(visualizationFunction)
+
+    # ---------------------------------------
+    # --- Create X0 (First Guess)
+    # ---------------------------------------
     opt.fromXToModel()
     opt.callObjectiveFunction()
 
-
-    #---------------------------------------
-    #--- Start Optimization
-    #---------------------------------------
+    # ---------------------------------------
+    # --- Start Optimization
+    # ---------------------------------------
     print("\n\nStarting optimization")
     opt.startOptimization()
-
-    
-
-
-
-
