842afbaea04231e9c8d552e77eb51efc4c96cf6a
miguelriemoliveira <m.riem.oliveira@gmail.com>
2019-06-08 20:13:50 +0100

    #35 finished todos ...

--- test/sensor_pose_json.py
+++ test/sensor_pose_json.py
@@ -152,14 +152,14 @@ if __name__ == "__main__":
     # ---------------------------------------
     # --- Define THE RESIDUALS
     # ---------------------------------------
-    # TODO continue with the todos ...
+    # TODO residuals: each error is computed after the sensor and the chessboard of a collection.
+    #  Thus, each error will be affected by the parameters tx,ty,tz,r1,r2,r3 of the sensor and the chessboar
 
-
-    for camera in dataset_cameras.cameras:
-        for aruco_id, aruco_detection in camera.rgb.aruco_detections.items():
-            params = opt.getParamsContainingPattern('C' + str(camera.name))
-            params.extend(opt.getParamsContainingPattern('A' + str(aruco_id)))
-            opt.pushResidual(name='C' + camera.name + 'A' + str(aruco_id), params=params)
+    # for camera in dataset_cameras.cameras:
+    #     for aruco_id, aruco_detection in camera.rgb.aruco_detections.items():
+    #         params = opt.getParamsContainingPattern('C' + str(camera.name))
+    #         params.extend(opt.getParamsContainingPattern('A' + str(aruco_id)))
+    #         opt.pushResidual(name='C' + camera.name + 'A' + str(aruco_id), params=params)
 
     print('residuals = ' + str(opt.residuals))
 
@@ -171,6 +171,9 @@ if __name__ == "__main__":
     # ---------------------------------------
     # --- SETUP THE VISUALIZATION FUNCTION
     # ---------------------------------------
+    # TODO we should plot the poses of the cameras and the chessboards in 3D,
+    #  and perhaps the sensor data with the projections.
+
     if args['view_optimization']:
         # position the windows in the proper place
         for i, camera in enumerate(dataset_cameras.cameras):
@@ -303,241 +306,7 @@ if __name__ == "__main__":
     print('\n')
     opt.printParameters(opt.xf, text='Final parameters')
 
-    ################################################################################################################
-    # Creating the optimized dataset
-    print('\n---------------------------------------------------------------------------------------------------------')
-    print('Creating optimized dataset...\n')
-
-    # STEP 0
-    # Compute the camera.depth.matrix usign the optimized camera.rgb.matrix and the depth_T_camera matrix (fixed)
-    # print('\n\n\nDepth matrix before')
-    # print(camera.depth.matrix)
-    for camera in opt.data_models['data_cameras'].cameras:
-        # TODO I don't understand why this multiplication should be like this. I have reached this through trial and
-        #  error. Must be clarified
-
-        # Save this first for point cloud transformation
-        camera.old_world_T_depth = np.linalg.inv(camera.depth.matrix)
-
-        # camera.depth.matrix =  np.dot(np.linalg.inv(opt.data_models['data_cameras'].depth_T_camera), np.linalg.inv(camera.rgb.matrix))
-        camera.depth.matrix = np.dot(camera.rgb.matrix, opt.data_models['data_cameras'].depth_T_camera)
-
-        camera.matrix = np.dot(camera.rgb.matrix, opt.data_models['data_cameras'].device_T_camera)
-
-        # print('Depth matrix')
-        # print(camera.depth.matrix)
-
-    # STEP 1
-    # Full copy of the dataset
-
-    # Get the dest folder name
-    if args['path_to_output_dataset'] is None:
-        folders = args['path_to_images'].split('/')
-        while '' in folders:
-            folders.remove('')
-
-        dataset_name = folders[-1]
-
-        args['path_to_output_dataset'] = args['path_to_images'] + '/../' + dataset_name + '_optimized'
-
-    # If an old version of the optimization exists
-    if os.path.exists(args['path_to_output_dataset']):
-        # Delete old folder
-        shutil.rmtree(args['path_to_output_dataset'])
-
-    # Create the new folder
-    os.mkdir(args['path_to_output_dataset'])
-
-    # Copy the images
-    for camera in opt.data_models['data_cameras'].cameras:
-        print()
-        src = args['path_to_images'] + '/' + os.path.basename(camera.rgb.filename)
-        dst = args['path_to_output_dataset'] + '/' + os.path.basename(camera.rgb.filename)
-        shutil.copyfile(src, dst)
-
-    # STEP 2
-    # Overwrite txt files with new transform
-    print('\nWriting new .txt files...')
-
-
-    def p6d(s):
-        """ Prints string with 6 decimal places """
-        return "{0:.6f}".format(s)
-
-
-    for camera in opt.data_models['data_cameras'].cameras:
-        # print("\nCamera " + str(camera.name) + ':')
-
-        world_T_camera_transposed = np.transpose(camera.rgb.matrix)
-        # world_T_camera_transposed = np.linalg.inv(camera.rgb.matrix)
-        world_T_depth_transposed = np.transpose(camera.depth.matrix)
-        world_T_device_transposed = np.transpose(camera.matrix)
-
-        # print("world_T_camera = " + str(world_T_camera_transposed))
-        # print("world_T_depth = " + str(world_T_depth_transposed))
-
-        txt_filename = args['path_to_output_dataset'] + '/' + camera.name.zfill(8) + '.txt'
-        fh = open(txt_filename, 'w')
-
-        # Write to file
-        fh.write('3\n')
-
-        for i in range(4):
-            fh.write(p6d(world_T_camera_transposed[i][0]) + ' ' + p6d(world_T_camera_transposed[i][1]) + ' ' + p6d(
-                world_T_camera_transposed[i][2]) + ' ' + p6d(world_T_camera_transposed[i][3]) + '\n')
-
-        for i in range(4):
-            fh.write(p6d(world_T_depth_transposed[i][0]) + ' ' + p6d(world_T_depth_transposed[i][1]) + ' ' + p6d(
-                world_T_depth_transposed[i][2]) + ' ' + p6d(world_T_depth_transposed[i][3]) + '\n')
-
-        for i in range(4):
-            fh.write(p6d(world_T_device_transposed[i][0]) + ' ' + p6d(world_T_device_transposed[i][1]) + ' ' + p6d(
-                world_T_device_transposed[i][2]) + ' ' + p6d(world_T_device_transposed[i][3]) + '\n')
-
-        fh.write(str(camera.rgb.stamp))
-        fh.close()
-        print('Transformations writen to ' + txt_filename)
-
-    # STEP 3
-    # point clouds World to depth camera ref frame using old transform
-    # then: point cloud from depth frame to world using new (optimized transform)
-
-    # Colour map initialization
-    cmap = cm.tab10(np.linspace(0, 1, 10))
-
-    # Structure to hold all point clouds
-    all_point_clouds = []
-    all_old_point_clouds = []
-
-    print("\nWriting new .ply files...")
-
-    for cam_idx, camera in enumerate(dataset_cameras.cameras):
-
-        # print('\nCamera ' + camera.name + ':')
-
-        # Ply file corresponding to current camera
-        ply_input_filename = args['path_to_images'] + '/' + camera.name.zfill(8) + '.ply'
-        # print('Read pointcloud from ' + ply_input_filename)
-
-        # Read vertices from point cloud
-        imgData = plyfile.PlyData.read(ply_input_filename)["vertex"]
-        num_vertex = len(imgData['x'])
-
-        # Create array of read 3d points                      add 1 to make homogeneous
-        xyz = np.c_[imgData['x'], imgData['y'], imgData['z'], np.ones(shape=(imgData['z'].size, 1))]
-        # Create array of read normals                            add 1 to make homogeneous
-        # nxyz = np.c_[imgData['nx'], imgData['ny'], imgData['nz'], np.ones(shape=(imgData['nz'].size, 1))]
-
-        # print('Computing point cloud transformations...')
-
-        # The local point clouds (ply files) are stored in openGL coordinates.
-        opengl2opencv = np.zeros((4, 4))
-        opengl2opencv[0, :] = [1, 0, 0, 0]
-        opengl2opencv[1, :] = [0, 0, 1, 0]
-        opengl2opencv[2, :] = [0, -1, 0, 0]
-        opengl2opencv[3, :] = [0, 0, 0, 1]
-        opencv2opengl = np.linalg.inv(opengl2opencv)
-
-        # old_world_T_depth = np.linalg.inv(camera.depth.matrix)
-
-        depth_T_camera = dataset_cameras.depth_T_camera
-
-        # This is after optimization
-        camera_T_world = camera.rgb.matrix
-
-        # Compute the transformation from the old openGL world to new openGL world
-        # Convert from OpenGL to OpenCV: Apply opengl2opencv conversion
-        # Go from world to depth through old transformation: Apply old_world_T_depth transformation
-        # Go from depth to camera: Apply depth_T_camera transformation
-        # Go from camera to new world: Apply optimized camera_T_world transformation
-        T = np.dot(opencv2opengl,
-                   np.dot(camera_T_world, np.dot(depth_T_camera, np.dot(camera.old_world_T_depth, opengl2opencv))))
-
-        pointsInNewWorld = np.transpose(np.dot(T, np.transpose(xyz)))
-
-        # Use colour map on new point clouds
-        r, g, b = (cmap[cam_idx % 10, 0:3] * 255)
-        r, g, b = int(r), int(g), int(b)
-
-        # Write to the .ply file
-        ply_output_filename = args['path_to_output_dataset'] + '/' + camera.name.zfill(8) + '.ply'
-        file_object = open(ply_output_filename, "w")
-
-        # Write file header information
-        writePlyHeader(file_object, num_vertex)
-
-        # Write the new points
-        for j in range(num_vertex):
-            line = \
-                str(pointsInNewWorld[j][0]) + ' ' + str(pointsInNewWorld[j][1]) + ' ' + str(pointsInNewWorld[j][2]) \
-                + ' ' + \
-                str(r) + ' ' + str(g) + ' ' + str(b) \
-                + '\n'
-
-            # str(normalsInNewWorld[j][0]) + ' ' + str(normalsInNewWorld[j][1]) + ' ' + str(normalsInNewWorld[j][2]) \
-            # + ' ' + \
-
-            # Write to ply file
-            file_object.write(line)
-
-            # Add line to merge
-            all_point_clouds.append(line)
-
-            # Get old line to merge clouds before optimization
-            oldLine = \
-                str(xyz[j][0]) + ' ' + str(xyz[j][1]) + ' ' + str(xyz[j][2]) \
-                + ' ' + \
-                str(r) + ' ' + str(g) + ' ' + str(b) \
-                + '\n'
-
-            # str(nxyz[j][0]) + ' ' + str(nxyz[j][1]) + ' ' + str(nxyz[j][2]) \
-            # + ' ' + \
-
-            all_old_point_clouds.append(oldLine)
-
-        file_object.close()
-
-        print('Pointcloud written to ' + ply_output_filename)
-
-    ################################################################################################################
-    # Merging pointclouds
-
-    # Write to the .ply file
-    ply_combined_output_filename = args['path_to_output_dataset'] + '/original_clouds.ply'
-    file_object = open(ply_combined_output_filename, "w")
-
-    # Write file header information
-    writePlyHeader(file_object, str(len(all_old_point_clouds)))
-
-    # Write the points
-    for point_cloud in all_old_point_clouds:
-        file_object.write(str(point_cloud))
-
-    print('\nOriginal pointclouds merged to ' + ply_combined_output_filename)
-
-    # Downsampling of combined point clouds
-    # tmpPc = read_point_cloud(ply_combined_output_filename)
-    # mergedOriginalPointClouds = voxel_down_sample(tmpPc, 0.05)
-    # draw_geometries([mergedOriginalPointClouds])
-    # write_point_cloud(ply_combined_output_filename, mergedOriginalPointClouds)
-    # print('\nFile ' + ply_combined_output_filename + ' was downsampled')
-
-    # Write to the .ply file
-    ply_combined_output_filename = args['path_to_output_dataset'] + '/optimized_clouds.ply'
-    file_object = open(ply_combined_output_filename, "w")
-
-    # Write file header information
-    writePlyHeader(file_object, str(len(all_point_clouds)))
-
-    # Write the points
-    for point_cloud in all_point_clouds:
-        file_object.write(str(point_cloud))
-
-    print('Optimized pointclouds merged to ' + ply_combined_output_filename)
-
-    # Downsampling of combined point clouds
-    # tmpPc = read_point_cloud(ply_combined_output_filename)
-    # mergedOriginalPointClouds = voxel_down_sample(tmpPc, 0.05)
-    # draw_geometries([mergedOriginalPointClouds])
-    # write_point_cloud(ply_combined_output_filename, mergedOriginalPointClouds)
-    # print('\nFile ' + ply_combined_output_filename + ' was downsampled')
+    # ---------------------------------------
+    # --- Save Results
+    # ---------------------------------------
+    # Todo should be saved back to a json or directly to xacro?
