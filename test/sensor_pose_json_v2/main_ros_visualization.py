948ce133a5e73be48b3103138c61502f75cb3984
Miguel Armando Riem Oliveira <m.riem.oliveira@gmail.com>
2019-12-10 12:23:32 +0000

    Close #81 creating a representation of initial sensor poses.

--- test/sensor_pose_json_v2/main_ros_visualization.py
+++ test/sensor_pose_json_v2/main_ros_visualization.py
@@ -170,17 +170,12 @@ def main():
     print('Loaded dataset containing ' + str(len(dataset_sensors['sensors'].keys())) + ' sensors and ' + str(
         len(dataset_sensors['collections'].keys())) + ' collections.')
 
-
     # transform_pool = dataset_sensors['collections']['0']['transforms']
     # print(utilities.getTransform('base_footprint', 'top_center_rgbd_camera_depth_optical_frame', transform_pool))
     # exit(0)
-
     # ---------------------------------------
     # --- SETUP OPTIMIZER
     # ---------------------------------------
-
-
-
     opt = OptimizationUtils.Optimizer()
     opt.addModelData('dataset_sensors', dataset_sensors)
     opt.addModelData('dataset_chessboard_points', dataset_chessboard_points)
@@ -317,8 +312,8 @@ def main():
     # ---------------------------------------
     # --- Start Optimization
     # ---------------------------------------
-    opt.startOptimization(
-        optimization_options={'ftol': 1e-4, 'xtol': 1e-8, 'gtol': 1e-5, 'diff_step': 1e-4, 'x_scale': 'jac'})
+    opt.startOptimization(optimization_options={'ftol': 1e-4, 'xtol': 1e-8,'gtol': 1e-5, 'diff_step': 1e-4,
+                                                'x_scale': 'jac'})
 
     print('\n-----------------')
     opt.printParameters(opt.x0, text='Initial parameters')
