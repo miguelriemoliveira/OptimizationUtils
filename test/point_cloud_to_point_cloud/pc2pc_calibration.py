620000156327b77843a29c66e80a173766e2c604
Paulo Dias <pmdjdias@gmail.com>
2020-03-18 14:00:18 +0000

    Translation optimization OK

--- test/point_cloud_to_point_cloud/pc2pc_calibration.py
+++ test/point_cloud_to_point_cloud/pc2pc_calibration.py
@@ -12,6 +12,7 @@ import argparse  # to read command line arguments
 import math
 
 import numpy as np
+import copy as cp
 from functools import partial
 import matplotlib.pyplot as plt
 import open3d as o3d
@@ -38,7 +39,50 @@ class SensorTransforms:
     def __init__(self):
         self.t = np.array([0.0, 0.0, 0.0])
         # self.r = np.array([0.0, 0.0, 0.0])
-        
+
+######################################
+# function to subsample a pointcloud #
+######################################
+def subSamplePointCloud(n,in_pc):
+    
+    points1 = np.array(in_pc.points)
+    # print(in_pc.shape[0])
+    # print(in_pc.shape[1])
+    
+    little_points = np.empty(shape = ( (points1.shape[0]/n,points1.shape[1]) ))
+    
+    ind = 0
+    for i in np.arange(0,len(little_points)):
+        little_points[i]= points1[ind]
+        ind += n
+
+
+    # print(little_points.shape)
+    
+    little_cloud = o3d.PointCloud()
+    little_cloud.points = o3d.Vector3dVector(little_points)
+
+    # print("writing little cow")
+    # o3d.io.write_point_cloud("./test/point_cloud_to_point_cloud/cow.ply",little_cloud) # Write the point cloud
+
+    return little_cloud
+
+#######################################
+# Add Gaussian Noise to a point cloud #
+#######################################
+def addNoise(max_error, cloud):
+    
+    cloudPoints = np.array(cloud.points)
+    noisy_points = np.empty(shape = (np.array(cloudPoints.shape) ))
+    
+    for i in np.arange(0,len(noisy_points)):
+        rand_error = np.random.random_sample( (1, 3) ) * max_error
+        noisy_points[i] = cloudPoints[i] + rand_error
+
+    #print(noisy_points.shape)
+
+    return noisy_points
+
 
 if __name__ == "__main__":
 
@@ -59,17 +103,16 @@ if __name__ == "__main__":
     # point cloud 1
     print("Read the point cloud")
     pcd1 = o3d.io.read_point_cloud("./test/point_cloud_to_point_cloud/cow.ply") # Read the point cloud
-    #pcd1 = o3d.io.read_point_cloud("./test/point_cloud_to_point_cloud/little_cow.ply") # Read the point cloud
     pcd1.paint_uniform_color([1, 0, 0])
 
-    pcd2 = o3d.io.read_point_cloud("./test/point_cloud_to_point_cloud/cow.ply") # Read the point cloud
-    #pcd2 = o3d.io.read_point_cloud("./test/point_cloud_to_point_cloud/little_cow.ply") # Read the point cloud
-
-    # pc2 is pc1 transformed
-    # trans_init = np.asarray([[0.862, 0.011, -0.507, 2],
-    #                          [-0.139, 0.967, -0.215, 1],
-    #                          [0.487, 0.255, 0.835, 2], 
-    #                          [0.0, 0.0, 0.0, 1.0]])
+    #smaller version of cow
+    pcd1 = subSamplePointCloud(5,pcd1)
+    
+    # pcd2 = o3d.io.read_point_cloud("./test/point_cloud_to_point_cloud/cow.ply") # Read the point cloud
+    # pcd2 = subSamplePointCloud(5,pcd2)
+    pcd2 = cp.deepcopy(pcd1)
+    #downpcd = pcd.voxel_down_sample(voxel_size=0.05)
+    
     trans_init = np.asarray([[0.862, 0.011, -0.507, 0.2],
                              [-0.139, 0.967, -0.215, 0.1],
                              [0.487, 0.255, 0.835, 0.2], 
@@ -77,24 +120,11 @@ if __name__ == "__main__":
 
     # random noise in pc2    
     pcd2.transform(trans_init)
-
-    points2 = np.array(pcd2.points)
-    print(points2.shape[0])
-    print(points2.shape[1])
-    
-    noisy_points = np.empty(shape = (points2.shape))
-    #noisy_points = np.empty(shape=(5,2))
-    
-    for i in np.arange(0,len(points2)):
-        rand_error = np.random.random_sample( (1, 3) ) * RANDOM_ERROR
-        noisy_points[i] = points2[i] + rand_error
-
-
-    print(noisy_points.shape)
-
+    noisy_points = addNoise(RANDOM_ERROR,pcd2)
 
     noisy_cloud = o3d.PointCloud()
     noisy_cloud.points = o3d.Vector3dVector(noisy_points)
+
     
    
     # ---------------------------------------
@@ -179,12 +209,14 @@ if __name__ == "__main__":
         # transformMatrix remember to initialize at identity
         # Check if data type is needed
         # ang_loop = np.array(sensorTransforms.r, dtype=np.float64)
-        tr_loop = np.array(sensorTransforms.t, dtype=np.float64)
+        # tr_loop = np.array(sensorTransforms.t, dtype=np.float64)
+        tr_loop = np.array([sensorTransforms.t[0],sensorTransforms.t[1],sensorTransforms.t[2]])
 
         # otf = tf.compose_matrix(scale=None, shear=None, angles=ang_loop, translate=tr_loop, perspective=None)
         otf = tf.compose_matrix(scale=None, shear=None, angles=None, translate=tr_loop, perspective=None)
-
-        y = noisy_cloud.transform(otf)
+        
+        y = cp.deepcopy(noisy_cloud)
+        y = y.transform(otf)
 
         sourcepts = np.array(y.points)
         targetpts = np.array(pcd1.points)
@@ -208,7 +240,7 @@ if __name__ == "__main__":
 
 
         # error.append(err)
-        print("first 5 errors = " + str(error[0:5]))
+        # print("first 5 errors = " + str(error[0:5]))
         return error
 
 
@@ -238,8 +270,9 @@ if __name__ == "__main__":
     vis = o3d.Visualizer()
     vis.create_window()
     vis.add_geometry(pcd1)
-    noisy_cloud.paint_uniform_color([0, 1, 0])
-    vis.add_geometry(noisy_cloud)
+    y = cp.deepcopy(noisy_cloud)
+    y.paint_uniform_color([0, 1, 0])
+    vis.add_geometry(y)
     vis.update_renderer()
     print("Lock in open3D windows (vis)")
     vis.run()
@@ -263,19 +296,19 @@ if __name__ == "__main__":
 
         # ang = np.array([sensorTransforms.r[0],sensorTransforms.r[1],sensorTransforms.r[2]])
         tr = np.array([sensorTransforms.t[0],sensorTransforms.t[1],sensorTransforms.t[2]])
-        print("Translation:")
-        print(tr)
+        # print("Translation:")
+        # print(tr)
 
         Mv = tf.compose_matrix(scale=None, shear=None, angles=None, translate=tr, perspective=None)
         # Mv = tf.compose_matrix(scale=None, shear=None, angles=ang, translate=tr, perspective=None)
     
-        print("Transformation Matrix: ") 
-        print(Mv)
-
-        y = noisy_cloud.transform(Mv)
+        # print("Transformation Matrix: ") 
+        # print(Mv)
+        y = cp.deepcopy(noisy_cloud)
+        y = y.transform(Mv)
         
         y.paint_uniform_color([0, 0, 1])
-        vis.add_geometry(y)
+        # vis.add_geometry(y)
         vis.update_renderer()
         vis.run()
 
@@ -305,12 +338,26 @@ if __name__ == "__main__":
 
     print("\n\nStarting optimization")
     opt.startOptimization(
-        optimization_options={'x_scale': 'jac', 'ftol': 1e-4, 'xtol': 1e-4, 'gtol': 1e-4, 'diff_step': 1e-4})
+        optimization_options={'x_scale': 'jac', 'ftol': 1e-2, 'xtol': 1e-2, 'gtol': 1e-2, 'diff_step': 1e-2})
+
+    vis.destroy_window()
+    vis.create_window()
+    vis.add_geometry(pcd1)
+    pcd1.paint_uniform_color([1, 0, 0])
 
 
-    vis.update_renderer()
-    vis.run()
+    tr = np.array([sensorTransforms.t[0],sensorTransforms.t[1],sensorTransforms.t[2]])
+  
+    Mv = tf.compose_matrix(scale=None, shear=None, angles=None, translate=tr, perspective=None)
+    
+    y = cp.deepcopy(noisy_cloud)
+    y = y.transform(Mv)
+        
+    y.paint_uniform_color([0, 1, 0])
+    o3d.visualization.draw_geometries([pcd1, y])
+    # vis.update_renderer()
+    # vis.run()
 
-    wm = KeyPressManager.KeyPressManager.WindowManager()
-    if wm.waitForKey():
-        exit(0)
+    # wm = KeyPressManager.KeyPressManager.WindowManager()
+    # if wm.waitForKey():
+    #     exit(0)
